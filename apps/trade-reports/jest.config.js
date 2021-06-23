8ca1660f80d493d3dd030c9f7fd694bc5bf73248
Alexander Kireev <farvistadw@gmail.com>
2021-06-23 05:31:46 +0300

    initial commit

---
+++ apps/trade-reports/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'trade-reports',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/trade-reports',
+};
